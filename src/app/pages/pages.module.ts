c48cd0ed637c63f011fab6abd87a1d5942b0dfe2
julian ferr <julian_691@hotmail.com>
2018-03-14 14:10:33 +0100

    Primer commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+
+// Rutas HIJAS
+import { PAGES_ROUTES } from './pages.routes';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+import { SharedModule } from '../shared/shared.module';
+
+
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
